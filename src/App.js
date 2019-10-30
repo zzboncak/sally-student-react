fec0b2877134dcb0c1b314442ee236b76aa89294
Zachary <zachary.zboncak@gmail.com>
2019-10-29 22:11:50 -0500

    rendered the resume to the page using react

--- src/App.js
+++ src/App.js
@@ -1,25 +1,39 @@
 import React from 'react';
-import logo from './logo.svg';
+import image from './sally-student.jpg';
 import './App.css';
 
-function App() {
+function App(props) {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <main>
+      <section className="top">
+        <header>
+          <h1>{props.name}</h1>
+        </header>
+      </section>
+      <div className="contact-picture-flex">
+        <section className="contact">
+          <h2>Contact Info</h2>
+          <address>
+            <a href="mailto:sally.student@gmail.com">sally.student@gmail.com</a><br />
+            <a href="tel:+16789234443">(678) 923-4443</a>
+          </address>
+        </section>
+        <section className="picture">
+          <img src={image} alt="This is a picture of me"/>
+        </section>
+      </div>
+      <section className="work">
+        <h2>Work Experience</h2>
+        <p className="job"><strong>Full-stack Developer -- Google (2013-2016)</strong></p>
+        <p className="description">In this job I maintained Google's front and back end operations.</p>
+        <p className="job"><strong>Intern -- Google (2011-2013)</strong></p>
+        <p className="description">In this role I was "Okay, Google" for 3 years, responding to everyone's questions.</p>
+      </section>
+      <section className="education">
+        <h2>Education Background</h2>
+        <p className="degree"><strong>B.S. In Computer Science -- Biola University, 2012</strong></p>
+      </section>
+    </main>
   );
 }
 
